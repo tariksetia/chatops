b1c991aa9825ace83d27e2de966c71788ca69ba6
Setia <tarik@byondreal.net>
2017-10-20 01:50:27 +0530

    User.consersationdata.name

--- dialogGreeting.js
+++ dialogGreeting.js
@@ -3,20 +3,20 @@ var builder = require('botbuilder');
 module.exports = [
     (session,args) => {
         var name = builder.EntityRecognizer.findEntity(args.entities, 'greeting.Name');
-        if (name && !session.userData.name){
-            session.userData.name = name.entity;
-            session.endDialog(`Hi! ${session.userData.name}. How can I help?`);
+        if (name && !session.conversationData.name){
+            session.conversationData.name = name.entity;
+            session.endDialog(`Hi! ${session.conversationData.name}. How can I help?`);
         }
-        else if (!session.userData.name){
+        else if (!session.conversationData.name){
             builder.Prompts.text(session,"Hi! What's your name?");
         }else {
-            session.endDialog(`Hi! ${session.userData.name}`);
+            session.endDialog(`Hi! ${session.conversationData.name}`);
         }
     },
     (session, results, next) => {
         if (results.response) {
-            session.userData.name = results.response;
-            session.endDialog(`Hi! ${session.userData.name}`);
+            session.conversationData.name = results.response;
+            session.endDialog(`Hi! ${session.conversationData.name}`);
         }
     }
     
