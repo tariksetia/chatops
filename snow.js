d61c38078e86597e03e2e477d96f3fe00404b0a3
mailforsachin <mailforsachin@gmail.com>
2018-02-16 19:26:17 +0530

    updated url
    fixed

--- snow.js
+++ snow.js
@@ -3,7 +3,7 @@ var request = require('request');
 module.exports ={
 
     createSnowTicket:(data,session, builder, onSuccess, onError) => {
-        var urlString = 'https://dev12965.service-now.com/api/now/v1/table/incident';
+        var urlString = 'https://dev27737.service-now.com/api/now/v1/table/incident';
             var options = {
                 url: urlString,
                 method: 'POST',
@@ -13,7 +13,7 @@ module.exports ={
                 Type: 'application/json',
                 auth: {
                     'user': 'admin',
-                    'pass': 'xa75LBhW6wVg'
+                    'pass': 'ghBSKDy4Y3wr'
                 }
             };
 
@@ -31,7 +31,7 @@ module.exports ={
             request(options, callback);
     },
     getSnowTicket : (session, number, onSucess, onError) => {
-        var urlString = 'https://dev12965.service-now.com/api/now/v2/table/incident?sysparm_limit=1&number='+number;
+        var urlString = 'https://dev27737.service-now.com/api/now/v2/table/incident?sysparm_limit=1&number='+number;
         var options = {
             url: urlString,
             method: 'GET',
@@ -40,7 +40,7 @@ module.exports ={
             Type: 'application/json',
             auth: {
                 'user': 'admin',
-                'pass': 'xa75LBhW6wVg'
+                'pass': 'ghBSKDy4Y3wr'
             }
         };
 
